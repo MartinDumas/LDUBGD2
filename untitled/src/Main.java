9fc37f445d718e72f6a126907275f598e4317787
MartinDumas <126817324+MartinDumas@users.noreply.github.com>
2023-03-02 22:26:54 +0200

    Лабораторна 2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        TASK1
+      int a=21;
+      int b=3;
+        System.out.println(a/b);
+        System.out.println(a%b);
+//        TASK2
+        int c=21;
+        int d= c/10;
+        int e= c%10;
+        int sum=d+e;
+        System.out.println(c);
+        System.out.println("Сума двохзначного числа:" + sum);
+//        TASK3
+     double p=21.6d;
+      System.out.println( Math.round(p));
+
+
     }
 }
